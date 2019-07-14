55451f164ed37f6f5ecd8b435181c785accd39d3
Ashiq Fardus <ashiqfardus@hotmail.com>
2019-07-14 14:45:52 +0600

    Color migration added

---
+++ database/migrations/2019_07_14_084112_create_colors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateColorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('colors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('theme_primary');
+            $table->string('theme_secondary');
+            $table->string('header_footer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('colors');
+    }
+}
