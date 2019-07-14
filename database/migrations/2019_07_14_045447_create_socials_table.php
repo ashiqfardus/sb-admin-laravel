a9e99f32b95a7d38a99f7315272a0308844148b1
Ashiq Fardus <ashiqfardus@hotmail.com>
2019-07-14 11:22:40 +0600

    Social Model & migration added

---
+++ database/migrations/2019_07_14_045447_create_socials_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socials', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('facebook');
+            $table->string('instagram');
+            $table->string('google');
+            $table->string('twiter');
+            $table->string('linkedin');
+            $table->string('youtube');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('socials');
+    }
+}
